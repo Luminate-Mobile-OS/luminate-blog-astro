8bfed7b0e2b6c102c9fb97a101fd971d451b1cd4
Lucas Nguyen <lucasoAlt@outlook.com>
2023-07-21 21:26:18 +0200

    add netlify cms to astro page

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,8 +2,29 @@ import { defineConfig } from 'astro/config';
 import tailwind from "@astrojs/tailwind";
 
 import react from "@astrojs/react";
+import NetlifyCMS from 'astro-netlify-cms';
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [tailwind(), react()]
+  integrations: [tailwind(), react(), NetlifyCMS({
+    config: {
+      backend: {
+        name: 'git-gateway',
+        branch: 'main',
+      },
+      collections: [
+        {
+          name: 'posts',
+          label: 'Blog Posts',
+          folder: 'src/pages/posts',
+          create: true,
+          delete: true,
+          fields: [
+            { name: 'title', widget: 'string', label: 'Post Title' },
+            { name: 'body', widget: 'markdown', label: 'Post Body' },
+          ],
+        },
+      ],
+    },
+  }),]
 });
